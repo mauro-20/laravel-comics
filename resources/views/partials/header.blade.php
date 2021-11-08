baa596eaf0b78a71229c1acf3ac050e5969c9a4f
Mauro Tomasoni <mauro.tom.vn@gmail.com>
2021-11-08 09:28:34 +0100

    add header

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,3 +1,25 @@
 <header>
-  header
+  <div class="container">
+    <div class="logo">
+      <img src="{{asset("images/dc-logo.png")}}" alt="logo dc">
+    </div>
+    <nav class="nav-menu">
+      <ul>
+        <li><a href="#">characters</a></li>
+        <li><a href="#" class="active">comics</a></li>
+        <li><a href="#">movies</a></li>
+        <li><a href="#">tv</a></li>
+        <li><a href="#">games</a></li>
+        <li><a href="#">collectibles</a></li>
+        <li><a href="#">videos</a></li>
+        <li><a href="#">fans</a></li>
+        <li><a href="#">news</a></li>
+        <li><a href="#">shop</a></li>
+      </ul>
+    </nav>
+    <div class="search">
+      <input type="text" name="" id="" placeholder="Search">
+      <i class="fas fa-search"></i>
+    </div>
+  </div>
 </header>
